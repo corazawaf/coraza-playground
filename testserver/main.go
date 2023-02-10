0c50d73a39948ecf741000b1b0de5761e945cb67
José Carlos Chávez <jcchavezs@gmail.com>
2023-02-10 23:16:20 +0100

    chore: adds a check for when the build dir does not exist.

--- testserver/main.go
+++ testserver/main.go
@@ -3,10 +3,17 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 )
 
+const buildDir = "./www"
+
 func main() {
-	fs := http.FileServer(http.Dir("./www"))
+	if _, err := os.Stat(buildDir); os.IsNotExist(err) {
+		log.Fatalf("build dir %q does not exist", buildDir)
+	}
+
+	fs := http.FileServer(http.Dir(buildDir))
 	http.Handle("/", fs)
 
 	log.Println("Listening on http://localhost:3000/index.html")
